11d20ed7caebb5a67e05895d2a031978826f858b
Megawaldi <47193219+Megawaldi@users.noreply.github.com>
2019-08-14 21:16:58 +0200

    fzf neu

--- zsh/plugins/fzf.zsh
+++ zsh/plugins/fzf.zsh
@@ -1,14 +1,15 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/thor/Git-Repos/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/thor/Git-Repos/.fzf/bin"
-fi
+#if [[ ! "$PATH" == */home/thor/Git-Repos/.fzf/bin* ]]; then
+#  export PATH="${PATH:+${PATH}:}/home/thor/Git-Repos/.fzf/bin"
+#fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/thor/Git-Repos/.fzf/shell/completion.zsh" 2> /dev/null
+#[[ $- == *i* ]] && source "/home/thor/Git-Repos/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/thor/Git-Repos/.fzf/shell/key-bindings.zsh"
+#source "/home/thor/Git-Repos/.fzf/shell/key-bindings.zsh"
+source "/usr/share/fzf/key-bindings.zsh"
 export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
