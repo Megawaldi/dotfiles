7f050709b22aa9add05ceafc90213268499a44e4
Thor <Darklands@web.de>
2021-01-16 18:25:45 +0100

    new Alias anonsurf

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -71,6 +71,9 @@ alias emulator="/usr/share/android-sdk/tools/emulator"
 # Vim
 alias v="vim -p"
 
+#Anonsurf
+alias anonsurf='sudo anonsurf'
+
 # Git
 # This is currently causing problems (fails when you run it anywhere that isn't a git project's root directory)
 # alias vs="v `git status --porcelain | sed -ne 's/^ M //p'`"
